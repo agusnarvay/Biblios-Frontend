ea87bd870a495535e9d3eaa440b411745eab80cc
agusnarvay <anarvay93@gmail.com>
2025-09-19 23:30:07 +0400

    Login

--- src/paginas/login.jsx
+++ src/paginas/login.jsx
@@ -1,7 +1,68 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
+function Login() {
+  //Los uso para guardar lo que el usuario escribe
+  const [usuario, setUsuario] = useState("")
+  const [contrasena, setContrasena] = useState("")
+  const [error, setError] = useState(null)
 
-export function Login() {
-    return <h1>Log in</h1>
+  const navigate = useNavigate()
+
+  const enviarFormulario = async (e) => {
+    e.preventDefault()
+    setError(null)
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_EXPRESS}/api/login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ usuario, contrasena }),
+        }
+      )
+      const data = await response.json()
+
+      if (!response.ok) {
+        new Error(data.message || "Error al iniciar sesión")
+
+        console.log("Login exitoso:", data)
+        navigate("/perfil")
+      }
+    } catch (error) {
+      next(error)
     }
+  }
+
+  return (
+    <div className="contenedor-login">
+      <h2>Iniciar sesión</h2>
+      <form onSubmit={enviarFormulario}>
+        <div>
+          <label htmlFor="usuario">Usuario</label>
+          <input
+            type="text"
+            id="usuario"
+            value={usuario}
+            onChange={(e) => setUsuario(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="contrasena">Contrasena</label>
+          <input
+            type="contrasena"
+            id="contrasena"
+            value={contrasena}
+            onChange={(e) => setContrasena(e.target.value)}
+            required
+          />
+        </div>
+      </form>
+      {error && <p className="error-msg">{error}</p>}
+    </div>
+  )
+}
 
 export default Login
